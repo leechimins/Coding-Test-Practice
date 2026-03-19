6b6603e19a68fec3e477ab9c513b77caa3576b57
이지민 <145891038+leechimins@users.noreply.github.com>
2026-03-19 14:38:19 +0900

    [Bronze I] Title: 팰린드롬수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string num = "";
+	cin >> num;
+
+	while (num != "0") {
+		string res(num.rbegin(), num.rend());
+		if (num == res)
+			cout << "yes\n";
+		else
+			cout << "no\n";
+		cin >> num;
+	}
+
+	return 0;
+}
